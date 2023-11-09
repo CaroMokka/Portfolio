673b5c486315cf05a93180ab0044d29933d9d65a
carolina <ci.arayagonzalez@gmail.com>
2023-11-09 19:31:42 -0300

    style - About me new text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,16 @@ const About = () => {
 	return (
 		<div className="about-container text-uppercase">
 			<div className="about-description">
-				<h3 className="about-h3">Sobre mi</h3>
+				<h3 className="about-h3">De Frontend a Diseño UX-UI</h3>
 				<p className="about-p">
-				+1 año de experiencia como Frontend. Soy curiosa, apasionada y perseverante, amante del mundo digital. De Frontend a diseño UX/UI, me gusta explorar e investigar cosas nuevas, siempre con el foco hacia un crecimiento constante. Mis proyectos han estado relacionados al desarrollo web, lo que me ha permitido conocer y trabajar con una gran diversidad de personas, entre ellas diseñadores. Actualmente me encuentro becada cursando el Programa de Certificación UX de Google.
+				Soy Caro y vivo actualmente en Santiago de Chile.
+Llevo más de un año de experiencia en el mundo digital. Me considero curiosa en esencia, apasionada y perseverante. Ultimamente me encontré con algo llamado UX. Ha sido todo un descubrimiento maravilloso. Disfruto explorando e investigando nuevos mundos. Si bien mis proyectos están relacionados al desarrollo web, este recorrido me ha permitido conocer y trabajar con una diversidad de personas, entre ellas diseñador@s. Aquí es donde me encuentro ahora, con un encanto por el UX/UI. Actualmente me encuentro becada y cursando el Programa de Certificación UX de Google.
 				</p>
 			</div>
 			
-			<div className="about-img">
+			{/* <div className="about-img">
 				<img src={ImageProfile} alt="about" />
-			</div>
+			</div> */}
 		</div>
 	);
 };
