01a92b0c29361a07f1df0a75cb53e551eb802504
carolina <ci.arayagonzalez@gmail.com>
2023-11-10 18:57:05 -0300

    :art: Title cover modificated

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './Cover.css';
-import coverVideo from '../../media/coverVideo.mp4';
-
+import React from "react";
+import "./Cover.css";
+import coverVideo from "../../media/coverVideo.mp4";
+import nameTitle from "../../media/name-02.webp";
 
 const Cover = () => {
-	return (
-		<div className="cover-container">
-			<video className="video" src={coverVideo} autoPlay loop muted />
-			<h2 className="cover-h1">CAROLINA ARAYA GONZÁLEZ</h2>
-			<p className="cover-p">
-				Frontend | UX-UI Design {" "}
-			</p>
-		</div>
-	);
+  return (
+    <div className="cover-container">
+      <video className="video" src={coverVideo} autoPlay loop muted />
+      <div className='container-title'>
+        <img src={nameTitle} />
+      </div>
+      {/* <h2 className="cover-h1">CAROLINA ARAYA GONZÁLEZ</h2> */}
+	  <p className="cover-p">Frontend | UX-UI Design </p>
+	  {/* <h2 className="cover-a">OPEN TO WORK *</h2> */}
+    </div>
+  );
 };
 
 export default Cover;
-
