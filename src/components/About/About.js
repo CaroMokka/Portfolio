b6e30fac30791599f98bc9e5e21216464b5d135d
carolina <ci.arayagonzalez@gmail.com>
2023-11-13 19:36:06 -0300

    :art: Update about section
    add typo webp

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,36 @@
 import React from "react";
-import ImageProfile from "../../media/caro.jpg";
 import "./About.css";
+import ImageProfile from "../../media/caro.jpg";
+import titleAbout from "../../media/title-aboutme-01.webp";
 
 const About = () => {
-	return (
-		<div className="about-container text-uppercase">
-			<div className="about-description">
-				<h3 className="about-h3">De Frontend a Diseño UX-UI</h3>
-				<p className="about-p">
-				Soy Caro y vivo actualmente en Santiago de Chile.
-Llevo más de un año de experiencia en el mundo digital. Me considero curiosa en esencia, apasionada y perseverante. Ultimamente me encontré con algo llamado UX. Ha sido todo un descubrimiento maravilloso. Disfruto explorando e investigando nuevos mundos. Si bien mis proyectos están relacionados al desarrollo web, este recorrido me ha permitido conocer y trabajar con una diversidad de personas, entre ellas diseñador@s. Aquí es donde me encuentro ahora, con un encanto por el UX/UI. Actualmente me encuentro becada y cursando el Programa de Certificación UX de Google.
-				</p>
-			</div>
-			
-			{/* <div className="about-img">
+  return (
+    <div className="about-container text-uppercase">
+      <h3 className="about-h3">Sobre mi</h3>
+      <div className='wrapper-aboutme'>
+	  <div className="container-aboutme">
+        <img className="title-aboutme" src={titleAbout} />
+      </div>
+      <div className="about-description">
+        <p className="about-p">
+          Soy Caro y vivo actualmente en Santiago de Chile. Llevo más de un año
+          de experiencia en el mundo digital. Me considero curiosa en esencia,
+          apasionada y perseverante. Ultimamente me encontré con algo llamado
+          UX. Ha sido todo un descubrimiento maravilloso. Disfruto explorando e
+          investigando nuevos mundos. Si bien mis proyectos están relacionados
+          al desarrollo web, este recorrido me ha permitido conocer y trabajar
+          con una diversidad de personas, entre ellas diseñador@s. Aquí es donde
+          me encuentro ahora, con un encanto por el UX/UI. Actualmente me
+          encuentro becada y cursando el Programa de Certificación UX de Google.
+        </p>
+      </div>
+	  </div>
+
+      {/* <div className="about-img">
 				<img src={ImageProfile} alt="about" />
 			</div> */}
-		</div>
-	);
+    </div>
+  );
 };
 
 export default About;
